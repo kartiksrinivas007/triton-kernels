ac9fff360e72a92b1d0e12258d157f32ddb214b3
Kartik Kartik Srinivas <kartiksr@riddle.autonomy.ri.cmu.edu>
2025-10-08 23:33:53 +0000

    Add Conv2d kernel

--- kernels/simple_kernels.py
+++ kernels/simple_kernels.py
@@ -25,43 +25,124 @@ def add_constant_kernel(
     pass
 
 
-
 @triton.jit
 def add_along_first_axis_kernel(
-    x_ptr, 
-    z_ptr, 
-    
-    x_size_m, 
-    x_size_t, 
-    
-    x_stride_m, 
-    x_stride_t: tl.constexpr, 
-    
-    z_stride_m, 
-    
+    x_ptr,
+    z_ptr,
+    x_size_m,
+    x_size_t,
+    x_stride_m,
+    x_stride_t: tl.constexpr,
+    z_stride_m,
     BLOCK_SIZE_M: tl.constexpr,
     BLOCK_SIZE_T: tl.constexpr,
-): 
+):
 
-    pid = tl.program_id(axis = 0)
+    pid = tl.program_id(axis=0)
     offset_m = pid * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
 
     # need to define an accumulator here
     accumulator = tl.zeros((BLOCK_SIZE_M,), dtype=tl.float32)
-    for offset_t_base in tl.static_range(0, x_size_t, BLOCK_SIZE_T): #type: ignore
+    for offset_t_base in tl.static_range(0, x_size_t, BLOCK_SIZE_T):  # type: ignore
         offset_t = offset_t_base + tl.arange(0, BLOCK_SIZE_T)
 
         # the net offsets to load the x block are now known
         mask_x_inside = (offset_m[:, None] < x_size_m) & (offset_t[None, :] < x_size_t)
-        x = tl.load(x_ptr + offset_m[:, None] * x_stride_m + offset_t[None, :] * x_stride_t, mask=mask_x_inside)
-        accumulator += tl.sum(x, axis = 1) # sum over the other axis
+        x = tl.load(
+            x_ptr + offset_m[:, None] * x_stride_m + offset_t[None, :] * x_stride_t,
+            mask=mask_x_inside,
+        )
+        accumulator += tl.sum(x, axis=1)  # sum over the other axis
 
-    mask_z_inside = offset_m < x_size_m # the size of x and z match, and the offsets are the same 
-    # get the locations for z 
+    mask_z_inside = (
+        offset_m < x_size_m
+    )  # the size of x and z match, and the offsets are the same
+    # get the locations for z
     z_ptrs_to_store = z_ptr + offset_m * z_stride_m
     tl.store(z_ptrs_to_store, accumulator, mask=mask_z_inside)
     pass
 
-        
 
-    
+# Scalar flash attention kernel
+
+
+@triton.jit
+def conv2d_kernel(
+    x_ptr,
+    k_ptr,
+    z_ptr,
+    # sizes
+    x_size_b,
+    x_size_h: tl.constexpr,
+    x_size_w: tl.constexpr,
+    k_size_h: tl.constexpr,
+    k_size_w: tl.constexpr,
+    z_size_h: tl.constexpr,
+    z_size_w: tl.constexpr,
+    # strides
+    x_stride_b,
+    x_stride_h,
+    x_stride_w,
+    k_stride_h,
+    k_stride_w,
+    z_stride_b,
+    z_stride_h,
+    z_stride_w,
+    # tunable params (block sizes)
+    BLOCK_SIZE: tl.constexpr
+):
+
+    DEBUG = False
+
+    pid = tl.program_id(axis=0)
+    offset_b = pid * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+    mask_b = offset_b < x_size_b
+
+    offset_k_h = tl.arange(0, k_size_h)
+    offset_k_w = tl.arange(0, k_size_w)
+    k_ptrs = k_ptr + offset_k_h[:, None] * k_stride_h + offset_k_w[None, :] * k_stride_w
+    k_full = tl.load(k_ptrs) 
+    # we do not need masks here because we know the full thing sits in memory, this means that the load
+    # that is done must be a multiple of a power of 2, else this might cause issues
+
+    for offset_base_h in tl.static_range(0, z_size_h):  # type: ignore
+        for offset_base_w in tl.static_range(0, z_size_w):  # type: ignore
+
+            offsets_x_h = offset_base_h + tl.arange(0, k_size_h)
+            mask_x_h = offsets_x_h < x_size_h
+            offsets_x_w = offset_base_w + tl.arange(0, k_size_w)
+            mask_x_w = offsets_x_w < x_size_w
+
+            # load these offsets into memory
+            x_ptrs = (
+                x_ptr
+                + offset_b[:, None, None] * x_stride_b
+                + offsets_x_h[None, :, None] * x_stride_h
+                + offsets_x_w[None, None, :] * x_stride_w
+            )
+            mask_x_block = (
+                mask_b[:, None, None]
+                & mask_x_h[None, :, None]
+                & mask_x_w[None, None, :]
+            )
+            x_block = tl.load(x_ptrs, mask_x_block)
+
+            # broadcast and multiply the block with k
+            xk = x_block * k_full[None, :, :]
+            xk_sum = tl.sum(xk, axis = 1) # multi axis sum is not supported
+            xk_sum = tl.sum(xk_sum, axis = 1) 
+
+            # then store these in the appropriate location of z (single location per batch)
+            # the other 2 dims are constants
+            z_ptr_to_store = z_ptr + (offset_b * z_stride_b) + (offset_base_h * z_stride_h) + (offset_base_w * z_stride_w)
+
+            # only need validity for something that could potentially go outside, the last two are true by definition
+            mask_z_is_valid = offset_b < x_size_b
+
+            tl.store(z_ptr_to_store, xk_sum, mask_z_is_valid)
+
+            if pid == 1 and offset_base_h ==0 and offset_base_w == 0  and DEBUG:
+                tl.device_print("X block = ", x_block.shape[0])
+                tl.device_print("pid =  ", pid)
+            pass
+    pass
--- main.py
+++ main.py
@@ -4,28 +4,31 @@ import torch
 import jaxtyping
 import triton.language as tl
 
-from kernels.simple_kernels import add_constant_kernel, add_along_first_axis_kernel
-
+from kernels.simple_kernels import (
+    add_constant_kernel,
+    add_along_first_axis_kernel,
+    conv2d_kernel,
+)
 
 
 if __name__ == "__main__":
-    
+
     assert torch.cuda.is_available(), "CUDA must be avialble to run triton kernels"
 
     size = 1000
-    x =  torch.rand(size, device="cuda")
-    y =  torch.rand((size, size), device="cuda")
+    x = torch.rand(size, device="cuda")
+    y = torch.rand((size, size), device="cuda")
 
     output = torch.empty_like(x)
-    grid = lambda meta : (triton.cdiv(size, meta["BLOCK_SIZE"]), )
+    grid = lambda meta: (triton.cdiv(size, meta["BLOCK_SIZE"]),)
 
     BLOCK_SIZE = 16
     CONSTANT = 5
 
     # add_constant_kernel[grid](
-    #     x, 
-    #     output, 
-    #     x.shape[0], 
+    #     x,
+    #     output,
+    #     x.shape[0],
     #     x.stride(0),
     #     output.stride(0),
     #     BLOCK_SIZE=tl.constexpr(BLOCK_SIZE),
@@ -35,21 +38,62 @@ if __name__ == "__main__":
     # print(x)
     # print(output)
 
+    # grid_second = lambda meta : (triton.cdiv(size, meta["BLOCK_SIZE_M"]), )
+    # add_along_first_axis_kernel[grid_second](
+    #     y,
+    #     output,
+    #     size,
+    #     tl.constexpr(size),
+    #     y.stride(0),
+    #     y.stride(1),
+    #     output.stride(0),
+    #     BLOCK_SIZE_M=tl.constexpr(BLOCK_SIZE),
+    #     BLOCK_SIZE_T=tl.constexpr(BLOCK_SIZE),
+    # )
+    # print(y)
+    # print(output)
+    # print(output  - torch.sum(y, dim=1))
 
-    grid_second = lambda meta : (triton.cdiv(size, meta["BLOCK_SIZE_M"]), )
-    add_along_first_axis_kernel[grid_second](
-        y, 
-        output,
-        size, 
-        tl.constexpr(size), 
-        y.stride(0),
-        y.stride(1),
-        output.stride(0),
-        BLOCK_SIZE_M=tl.constexpr(BLOCK_SIZE),
-        BLOCK_SIZE_T=tl.constexpr(BLOCK_SIZE),
+    B = 200
+    H = 10
+    W = 10
+    KH = 2
+    KW = 2
+    x = torch.ones((B, H, W), device="cuda")
+    k = torch.randn((KH, KW), device="cuda")
+    z_real = torch.nn.functional.conv2d(
+        x[:, None, ...], k[None, None, ...]
+    ).squeeze(1)
+    z = torch.zeros_like(z_real)
+    print(z.shape)
+    grid_third = lambda meta: (triton.cdiv(B, meta["BLOCK_SIZE"]),)
+    
+    # pass these to the tensor then
+    conv2d_kernel[grid_third](
+        x,
+        k,
+        z,
+        # sizes
+        B,
+        tl.constexpr(H),
+        tl.constexpr(W),
+        tl.constexpr(KH),
+        tl.constexpr(KW),
+        tl.constexpr(H - KH + 1),
+        tl.constexpr(W - KW + 1),
+        # strides
+        x.stride(0),
+        x.stride(1),
+        x.stride(2),
+        k.stride(0),
+        k.stride(1),
+        z.stride(0),
+        z.stride(1),
+        z.stride(2),
+        # BLOCKS
+        BLOCK_SIZE=tl.constexpr(BLOCK_SIZE),
     )
-    print(y)
-    print(output)
-    print(output  - torch.sum(y, dim=1))
+
+    print(z == z_real)
 
     pass
