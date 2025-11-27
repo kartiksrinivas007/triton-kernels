9af6d6e934bcb60f532d7c0c0a7401ff8e365413
kartiksrinivas007 <kartiksr@andrew.cmu.edu>
2025-11-27 12:49:40 -0500

    Add Perplexing test for perplexing bug.
    The backward scan dA functions compute the gradient with respect to the
    A block in the diagonal multiplication (CB^T o L) X, except in our case
    there is no CB and no dt.
    
    The test file in this commit has an implementation of the forward pass
    and also an implementation of the backward pass (both in pytorch)
    
    The backward pass matches torch autograd of the forward pass
    but the gradients of the torch autograd do not match that of the mamba-2
    kernel or the modified emal kernel (the latter 2 match though!)

--- kernels/ema_kernels_bwd/ema_scan_da.py
+++ kernels/ema_kernels_bwd/ema_scan_da.py
@@ -8,6 +8,8 @@ from packaging import version
 @triton.autotune(
     configs=[
         triton.Config({'BLOCK_SIZE_M': 16, 'BLOCK_SIZE_N': 16}, num_stages=3, num_warps=4),
+        # triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 64}, num_stages=4, num_warps=8),
+        # triton.Config({'BLOCK_SIZE_M': 128, 'BLOCK_SIZE_N': 128}, num_stages=4, num_warps=8),
         # triton.Config({'BLOCK_SIZE_M': 32, 'BLOCK_SIZE_N': 64}, num_stages=3, num_warps=4),
         # triton.Config({'BLOCK_SIZE_M': 32, 'BLOCK_SIZE_N': 128}, num_stages=3, num_warps=4),
         # triton.Config({'BLOCK_SIZE_M': 64, 'BLOCK_SIZE_N': 32}, num_stages=3, num_warps=4),
@@ -53,7 +55,7 @@ def _ema_chunk_scan_bwd_ddAcs_stable_kernel(
     x_ptrs = x_ptr + (offs_n[None, :] * stride_x_seqlen + offs_k[:, None] * stride_x_token_dim)
     # dt_ptrs = dt_ptr + offs_n * stride_dt_csize
     # cb_ptrs = cb_ptr + (offs_m[:, None] * stride_cb_csize_m + offs_n[None, :] * stride_cb_csize_n)
-    ddAcs_ptrs = ddA_cumsum_ptr + offs_n * stride_ddA_cs_csize_n # moved ahead by one column
+    ddAcs_ptrs = ddA_cumsum_ptr + offs_n * stride_ddA_cs_csize_n 
     tl.store(ddA_cumsum_ptr, 0.0) # first gradient is zero (1 - p_1)
 
     chunk_size_limit = min(chunk_size, seqlen - pid_c * chunk_size)
@@ -61,7 +63,8 @@ def _ema_chunk_scan_bwd_ddAcs_stable_kernel(
     dout = tl.load(dout_ptrs, mask=(offs_m[:, None] < chunk_size_limit) & (offs_k[None, :] < token_dim), other=0.0)
     dA_cs_m = tl.load(dA_cumsum_ptr + offs_m * stride_dA_cs_csize, mask=offs_m < chunk_size, other=0.0).to(tl.float32)
     # Actually hi is (pid_m + 1) * BLOCK_SIZE_M - 1 but subtracting 1 makes it slower
-    lo, hi = 0, (pid_m + 1) * BLOCK_SIZE_M - 1
+    lo, hi = 0, (pid_m + 1) * BLOCK_SIZE_M
+    hi = min(hi, chunk_size) # Clamp hi to chunk_size
     # lo, hi = 0, chunk_size
     for start_n in range(lo, hi, BLOCK_SIZE_N):
         start_n = tl.multiple_of(start_n, BLOCK_SIZE_N) # NOTE: This is just a compiler directive that start_n is a multiple
@@ -112,7 +115,7 @@ def _ema_chunk_scan_bwd_ddAcs_stable(x, dA_cumsum, dout):
     _, nchunks, chunk_size = dA_cumsum.shape
     assert dout.shape == x.shape
     BLOCK_SIZE_M_min = 16
-    ddA_cumsum = torch.empty(batch, nchunks, triton.cdiv(chunk_size, BLOCK_SIZE_M_min),
+    ddA_cumsum = torch.zeros(batch, nchunks, triton.cdiv(chunk_size, BLOCK_SIZE_M_min),
                              chunk_size, device=x.device, dtype=torch.float32)
     grid_ddtcs = lambda META: (triton.cdiv(chunk_size, META['BLOCK_SIZE_M']), batch, nchunks)
 
@@ -126,6 +129,7 @@ def _ema_chunk_scan_bwd_ddAcs_stable(x, dA_cumsum, dout):
             dA_cumsum.stride(0), dA_cumsum.stride(1), dA_cumsum.stride(2),
             ddA_cumsum.stride(0), ddA_cumsum.stride(1), ddA_cumsum.stride(2), ddA_cumsum.stride(3),
             BLOCK_SIZE_K=16
+            # BLOCK_SIZE_K =  16
         )
     BLOCK_SIZE_M_actual = _ema_chunk_scan_bwd_ddAcs_stable_kernel.best_config.kwargs["BLOCK_SIZE_M"]
     n_valid_blocks = (chunk_size + BLOCK_SIZE_M_actual - 1) // BLOCK_SIZE_M_actual
--- kernels/tests/ema_bwd/test_ema_scan_da.py
+++ kernels/tests/ema_bwd/test_ema_scan_da.py
@@ -1,16 +1,41 @@
 import torch
 import triton.runtime.driver as driver
 from kernels.ema_kernels_bwd.ema_scan_da import _ema_chunk_scan_bwd_ddAcs_stable
+from kernels.mamba_kernels_bwd.mamba_chunk_scan_bwd_da import _chunk_scan_bwd_ddAcs_stable
 import random
+from einops import rearrange, repeat
 
 def ema_chunk_scan_cumsum_fwd(x_chunks, A, chunk_size):
     A_cs = torch.cumsum(A, dim=-1)  # (B,C,Q)
     scale_logits = A_cs[..., :, None] - A_cs[..., None, :]  # (B,C,Q,Q)
     scale = torch.exp(torch.minimum(scale_logits, torch.tensor(0.0, device=x_chunks.device)))  # (B,C,Q,Q)
+    mask = torch.tril(torch.ones(chunk_size, chunk_size,  device=x_chunks.device, dtype=x_chunks.dtype))
+    scale = scale * mask  # x is (B, C, Q, Q)
     output = torch.matmul(scale, x_chunks)  # (B, C, Q, T)
     return output   
 
 
+# This is a hand crafted backward function for the forward function above
+def ema_chunk_cumsum_bwd(x_chunks, A, dout, chunk_size):
+    A_cs = torch.cumsum(A, dim=-1)  # (B,C,Q)
+    scale_logits = A_cs[..., :, None] - A_cs[..., None, :]  # (B,C,Q,Q)
+    scale = torch.exp(torch.minimum(scale_logits, torch.tensor(0.0, device=x_chunks.device)))  # (B,C,Q,Q)
+    mask = torch.tril(torch.ones(chunk_size, chunk_size,  device=x_chunks.device, dtype=x_chunks.dtype))
+    scale = scale * mask  # x is (B, C, Q, Q)
+    # use the scale and mul with outputs
+    inter  = torch.matmul(dout, rearrange(x_chunks, "b c q t -> b c t q"))  # (B, C, Q, Q)
+    scale_grad = torch.mul(inter, scale) # exp(scale) was the multiplying factor
+    # zero out diagonals of scale_grad 
+    lower_mask = mask - torch.eye(chunk_size, device=x_chunks.device, dtype=x_chunks.dtype)
+    scale_grad = scale_grad * lower_mask  # (B,C,Q,Q)
+    # cumsum 
+    scale_grad_cumsum = torch.cumsum(scale_grad, dim=-1)  # (B,C,Q,Q)
+    # zero out lower triangle of the cumsum again
+    scale_grad_cumsum = scale_grad_cumsum * lower_mask  # zero out diagonals again
+    ddA = torch.sum(scale_grad_cumsum, dim=-2)  # (B,C,Q)
+    shifted = torch.zeros_like(ddA)
+    shifted[..., 1:] = ddA[..., :-1]
+    return shifted
 
 
 
@@ -22,6 +47,8 @@ class TestEmaChunkScanBwddA:
         cls.T = 512
         cls.SEQLEN = 8192
         cls.NCHUNKS = (cls.SEQLEN + cls.CHUNK - 1) // cls.CHUNK
+
+        torch.manual_seed(0)
 
         cls.x = torch.randn(cls.B, cls.SEQLEN, cls.T, device=cls.device, dtype=torch.float32)
         # Interpret dA_cumsum as (A_cs_last - A_cs_m); keep negative so exp(min(...,0)) stays in exp branch
@@ -35,6 +62,7 @@ class TestEmaChunkScanBwddA:
     
     def test_grad_matches_autograd_dA(self):
         # Autograd reference
+        torch.manual_seed(0)
 
         x_chunks = self.x.view(self.B, self.NCHUNKS, self.CHUNK, self.T)  # (B,C,Q,T)
         # scale_logits = self.dA_cumsum[..., -1, None] - self.dA_cumsum  # (B,C,Q)
@@ -45,16 +73,40 @@ class TestEmaChunkScanBwddA:
         loss.backward()
         dA = A_var.grad
 
+        dout_chunks = rearrange(self.dout, " b (c q) t -> b c q t", c=self.NCHUNKS, q=self.CHUNK)
+
+
         ddA_kernel  = _ema_chunk_scan_bwd_ddAcs_stable(
             self.x,
-            self.dA_cumsum,
+            self.dA_cumsum.detach(),
             self.dout,
         )
 
-        # No shift adjustment needed
-        expected = dA
+        ddA_kernel[..., 0] = 0.0  # because of shift in the kernel implementation
 
-        # breakpoint()
-        # this is a bit loose!! - I am unable to make this better 
-        assert torch.allclose(ddA_kernel, expected, atol=2e-1, rtol=2e-1) # type:ignore
+        dda_torch = ema_chunk_cumsum_bwd(
+            x_chunks,
+            self.A,
+            dout_chunks,
+            self.CHUNK,
+        )
+
+        HEADS = 4
+        dt = torch.ones(self.B, HEADS, self.NCHUNKS, self.CHUNK, device=self.device, dtype=torch.float32)
+        cb = torch.ones(self.B, self.NCHUNKS, 1, self.CHUNK, self.CHUNK, device=self.device, dtype=torch.float32)
+        a_cs_mamba = repeat(self.dA_cumsum, "b c q -> b h c q", h=HEADS)
+        dda_mamba = _chunk_scan_bwd_ddAcs_stable(
+            rearrange(self.x, " b l (h d) -> b l h d", h=HEADS),
+            dt,
+            a_cs_mamba,
+            rearrange(self.dout, " b l (h d) -> b l h d", h=HEADS),
+            cb,
+        )
+        dda_mamba = torch.sum(dda_mamba, dim=1)  # sum over heads
+
+        print("\n Max abs diff ddA kernel vs torch autograd:", torch.max(torch.abs(ddA_kernel - dA)).item())
+        print("Max abs diff ddA mamba vs torch autograd:", torch.max(torch.abs(dda_mamba - dA)).item())
+        print("Max abs diff ddA kernel vs mamba:", torch.max(torch.abs(ddA_kernel - dda_mamba)).item())
+        print("Max diff between hand crafted backward and autograd:", torch.max(torch.abs(dA - dda_torch)).item())
+        assert torch.allclose(ddA_kernel, dA, atol=2e-1, rtol=2e-1) # type:ignore
 
